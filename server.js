b72bbaa156f7ed564ae0948b7f5dcbb2e2377c9b
btwitsahsan <ahsan899289@gmail.com>
2025-02-14 23:53:19 +0500

    gsdgds

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const userRoute = require("./routes/userRoute");
@@ -15,39 +15,37 @@ const app = express();
 // MiddleWare
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(
-    app.use(
-        cors({
-            origin: "https://chat-web-frontend-rho.vercel.app",
-            credentials: true, // ✅ Required to send cookies
-            allowedHeaders: ["Content-Type", "Authorization"],
-            methods: ["GET", "POST", "PUT", "DELETE"],
-        })
-    );
+  cors({
+    origin: "https://chat-web-frontend-rho.vercel.app",
+    credentials: true, // ✅ Required to send cookies
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
 );
 
 const PORT = process.env.PORT || 5000;
 
 //  Routes
-app.get("/",(req,res)=>{
-    res.send("home page....")
-})
+app.get("/", (req, res) => {
+  res.send("home page....");
+});
 // User Routes
-app.use("/api/users", userRoute)
-app.use("/api/products", productRoute)
-app.use("/api/conversations", conversationRoute)
-app.use("/api/messages", messageRoute)
-app.use("/api/friends", friendRequestsRoute)
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/friends", friendRequestsRoute);
 
 //Error Middleware
 app.use(errorHandler);
 
 mongoose
-.connect(process.env.MONGO_URI)
-.then(()=>{
-    app.listen(PORT, ()=>{
-        console.log(`server running on port ${PORT}`)
-    })
-})
-.catch((err)=> console.log(err))
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
