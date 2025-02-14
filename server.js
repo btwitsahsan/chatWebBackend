55365ad3629f8cde1cc0b9787f4844c92f1e0c71
btwitsahsan <ahsan899289@gmail.com>
2025-02-14 17:27:06 +0500

    apply deployment api

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 app.use(
     cors({
-    origin: ["http://localhost:3000", "https://chat-web-frontend-rho.vercel.app/"],
+    origin: ["http://localhost:3000", "https://chat-web-frontend-rho.vercel.app"],
     credentials: true,
 }));
 
